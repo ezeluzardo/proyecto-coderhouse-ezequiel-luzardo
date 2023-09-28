666d07894ec0a118deb329d0e74c50f5b9f17a15
ezeluzardo <ezeluzardo@gmail.com>
2023-09-27 22:39:26 -0300

    Se modifico Contacto.js
    se modifico contacto.js, para agregar asincronismo

--- contacto.js
+++ contacto.js
@@ -1,36 +1,44 @@
-// Inicializa EmailJS 
-emailjs.init("ADIHRDij4Prttf7fB"); 
+
+// Inicializa EmailJS
+emailjs.init("ADIHRDij4Prttf7fB");
+
+// Función para enviar el formulario de manera asincrónica
+async function enviarFormulario() {
+    try {
+        // Obtiene los valores del formulario
+        const nombre = document.querySelector("[name='nombre']").value;
+        const apellido = document.querySelector("[name='apellido']").value;
+        const email = document.querySelector("[name='email']").value;
+        const telefono = document.querySelector("[name='telefono']").value;
+        const formControl = document.querySelector("[name='form-control']").value;
+
+        // Prepara los datos para enviar en el correo electrónico
+        const emailData = {
+            nombre,
+            apellido,
+            email,
+            telefono,
+            "form-control": formControl,
+        };
+
+        // Envia el formulario utilizando EmailJS y la plantilla de correo electrónico
+        const response = await emailjs.send("service_68gss55", "template_pmt8n5m", emailData);
+
+        console.log("Correo electrónico enviado con éxito", response);
+        alert("Correo electrónico enviado con éxito");
+
+        // Limpia los campos del formulario
+        document.getElementById("myForm").reset();
+    } catch (error) {
+        console.log("Error al enviar el correo electrónico", error);
+        alert("Error al enviar el correo electrónico");
+    }
+}
 
 // Agrega un listener para el formulario
-document.getElementById("myForm").addEventListener("submit", function(event) {
-    event.preventDefault(); 
+document.getElementById("myForm").addEventListener("submit", function (event) {
+    event.preventDefault();
 
-    // Obtiene los valores del formulario
-    const nombre = this.querySelector("[name='nombre']").value;
-    const apellido = this.querySelector("[name='apellido']").value;
-    const email = this.querySelector("[name='email']").value;
-    const telefono = this.querySelector("[name='telefono']").value;
-    const formControl = this.querySelector("[name='form-control']").value;
-
-    // Prepara los datos para enviar en el correo electrónico
-    const emailData = {
-        nombre,
-        apellido,
-        email,
-        telefono,
-        "form-control": formControl,
-    };
-
-    // Envia el formulario utilizando EmailJS y la plantilla de correo electrónico
-    emailjs.send("service_68gss55", "template_pmt8n5m", emailData)
-        .then(function(response) {
-            console.log("Correo electrónico enviado con éxito", response);
-            alert("Correo electrónico enviado con éxito");
-
-            // Limpia los campos del formulario
-            document.getElementById("myForm").reset();
-        }, function(error) {
-            console.log("Error al enviar el correo electrónico", error);
-            alert("Error al enviar el correo electrónico");
-        });
+    // Llama a la función para enviar el formulario de manera asincrónica
+    enviarFormulario();
 });
